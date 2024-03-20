9a7797f27268e0c622adad4a634fc82d7b370c9f
lakshmikanth-nageswar <lakshmikanth.nageswar@gmail.com>
2024-03-20 13:48:44 +0530

    initial drivers added

--- src/scritps/tele_mavros.py
+++ src/scritps/tele_mavros.py
@@ -1,1 +1,5 @@
 import rospy 
+from sensor_msgs.msg import Imu         #IMU Subscribe from Pixhawk
+from sensor_msgs.msg import NavSatFix
+# from geometry_msgs.msg import _Vector3  #GPS Subscribe from Pixhawk
+
---
+++ src/ydlidar_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lakshmikanth/catkin_ws/src/flight_testing_lab/src/ydlidar_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-11.4/bin:/home/lakshmikanth/.mujoco/mujoco210/bin:/usr/lib/nvidia:/home/lakshmikanth/anaconda3/condabin:/home/lakshmikanth/.juliaup/bin:/usr/lib/ccache:/home/lakshmikanth/ardupilot/Tools/autotest:/opt/gcc-arm-none-eabi-10-2020-q4-major/bin:/home/lakshmikanth/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda-11.4/bin/snap/bin:/home/lakshmikanth/.local/bin'
+export PWD='/home/lakshmikanth/catkin_ws/src/flight_testing_lab/src/ydlidar_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lakshmikanth/catkin_ws/src/flight_testing_lab/src/ydlidar_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/lakshmikanth/catkin_ws/src/flight_testing_lab/src/ydlidar_ws/src:$ROS_PACKAGE_PATH"
